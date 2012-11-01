61db93b156ff7ddfcb418cd30ea284effd6f80bf
HolyCow <mike.beasterfeld@gmail.com>
2012-11-01 09:23:40 -0500

    FizzBuzz for fun

---
+++ fizzbuzz.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+
+
+for (1..100) {
+
+   if ( $_ % 3 == 0 ) {
+	print "Fizz";
+   }
+
+   if ( $_ % 5 == 0 ) {
+      print "Buzz";
+   }
+
+   if ( $_ % 3 != 0 && $_ % 5 != 0 ) {
+      print "$_";
+   }
+
+   print "\n";
+}
+
